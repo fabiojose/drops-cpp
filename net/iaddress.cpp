d670b785bcb6a396d2f0377cd490cb78584b8e94
Fabio Jose <fabiojose@gmail.com>
2014-03-11 10:36:18 -0300

    linux: inet_pton();

--- net/iaddress.cpp
+++ net/iaddress.cpp
@@ -4,7 +4,12 @@ InetAddress::InetAddress(char* host){
     this->host = host;
 
     //configure the default host in the structure
-    address.sin_addr.s_addr = inet_addr(this->host);
+    //piece of code by OS
+    #ifdef _WIN32
+      address.sin_addr.s_addr = inet_addr(this->host);
+    #else
+      inet_pton(AF_INET, this->host, &address.sin_addr);
+    #endif
 }
 
 InetAddress::~InetAddress(){
